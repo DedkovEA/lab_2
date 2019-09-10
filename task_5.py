b54d8710e021ce56fdb95971809bcd8c82b645bc
Dedkov Eugene <dedkov.ea@phystech.edu>
2019-09-10 11:47:31 +0300

    Squares now is centered

--- task_5.py
+++ task_5.py
@@ -5,10 +5,14 @@ delta = 40
 first = 100
 square_number = 10
 
+t.penup()
+t.goto(-first/2,-first/2)
+t.pendown()
+
 for i in range(square_number):
 	for j in range(4):
 		t.forward(first+i*delta)
 		t.left(90)
 	t.penup()
-	t.goto(-delta*(i+1)/2,-delta*(i+1)/2)
+	t.goto(-delta*(i+1)/2 - first/2, -delta*(i+1)/2 - first/2)
 	t.pendown()
